99059fe9d30a637f556e4dc9ec06c8aee6163c60
Brandon Gupton <brandon.gupton@e-gineering.com>
2024-03-08 17:07:37 -0500

    Convert from sfdx to sf for dx-tools functions

--- devcontainer/dx-tools.sh
+++ devcontainer/dx-tools.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
 function dx-clean() {
-    tools_dir=$(find /workspaces -type d -path "*sfdx/tools")
+	tools_dir=$(find /workspaces -type d -path "*sfdx/tools")
 
-    if [ -z "$tools_dir" ]
-    then
-        echo "Unable to find tools directory"
-    else
-        while IFS= read -r line
+	if [ -z "$tools_dir" ]
+	then
+		echo "Unable to find tools directory"
+	else
+		while IFS= read -r line
 		do
 			rm -f "$line"/debug/logs/*.log
 			rm -f "$line"/testresults/apex/*.json
 		done <<< "$tools_dir"
-    fi
+	fi
 }
 
 function dx-details() {
-    if [ $# -ne 1 ]
-    then
-        sfdx org display
-    else
-        sfdx org display --target-org="$1"
-    fi
+	if [ $# -ne 1 ]
+	then
+		sf org display
+	else
+		sf org display --target-org="$1"
+	fi
 }
 
 function dx-list() {
-    sfdx org list
+	sf org list
 }
 
 function dx-open() {
-    if [ $# -ne 1 ]
-    then
-        sfdx open org
-    else
-        sfdx open org --target-org="$1"
-    fi
+	if [ $# -ne 1 ]
+	then
+		sf open org
+	else
+		sf open org --target-org="$1"
+	fi
 }
 
 function dx-test() {
-    sfdx apex run test -l RunLocalTests | grep -o '".*"' | sed 's/"//g' | sh
+	sf apex run test -l RunLocalTests | grep -o '".*"' | sed 's/"//g' | sh
 }
